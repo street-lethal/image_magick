e0188017ecb6440e66fbd5167606c04e869f05ab
street-lethal <street.lethal.57@gmail.com>
2025-04-14 01:16:46 +0900

    brighten

---
+++ scripts/unzip_and_brighten.sh
@@ -0,0 +1,1 @@
+docker-compose run --rm share sh -c "../scripts/unzip_and_brighten.sh"
---
+++ scripts_in_docker/unzip_and_brighten.sh
@@ -0,0 +1,15 @@
+#!/bin/ash
+ls -1 *.zip | sed -e 's/\.zip$//' > ../tmp/filename.txt
+while read line
+do
+  unzip "../shared/$line.zip" -d ../tmp
+  cd ../tmp
+  for j in *.jpg
+  do
+    magick "$j" -brightness-contrast 5% brighten_"$j"
+  done
+  cd -
+  magick ../tmp/brighten_*.jpg "$line.pdf"
+  rm ../tmp/*.jpg
+  chown 1000:1000 "$line.pdf"
+done < ../tmp/filename.txt
